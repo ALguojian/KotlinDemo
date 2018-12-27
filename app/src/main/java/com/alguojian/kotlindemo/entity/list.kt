51fd9362946247e87ed295d65abccf40b83c9908
ALguojian <3324883037@qq.com>
2018-12-27 19:01:22 +0800

    ..

---
+++ app/src/main/java/com/alguojian/kotlindemo/entity/list.kt
@@ -0,0 +1,14 @@
+package com.alguojian.kotlindemo.entity
+
+/**
+ * ${Descript}
+ *
+ * @author alguojian
+ * @date 2018/12/27
+ */
+class list{
+
+    var list= listOf(1,2,3)
+    var map = mapOf("page" to 1,"size" to 20)
+
+}
